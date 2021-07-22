d088acfa4f2f31b278a35ed9b09e6987cfd4536c
vapaus <snaqerou@gmail.com>
2021-07-22 19:45:01 +0200

    added apollo graphql

---
+++ src/apollo.ts
@@ -0,0 +1,13 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+
+const httpLink = createHttpLink({ uri: "https://rickandmortyapi.com/graphql" });
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({ link: httpLink, cache });
+
+export { apolloClient };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { router } from "./router";
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
 import App from "./App.vue";
+import { apolloClient } from "./apollo";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
-const app = createApp(App);
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
 
 app.use(router);
 app.mount("#app");
