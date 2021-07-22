91f5f71c4b7cda5e7669645e8ee272f2c8c1ede9
vapaus <snaqerou@gmail.com>
2021-07-22 23:15:35 +0200

    table component handle styles and loading, no data states

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,25 +1,37 @@
 <template>
-  <table class="table">
-    <thead>
-      <th v-for="column in columns" :key="column.title">{{ column.title }}</th>
-    </thead>
-    <tbody>
-      <tr v-for="item in data" :key="item">
-        <td
-          v-for="column in columns"
-          :key="column.title"
-          :data-label="column.title"
-        >
-          <div v-if="!column.customRender">
-            {{ item[column.dataIndex] }}
-          </div>
-          <div v-else>
-            <slot :name="column.customRender" :value="item" />
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="info" v-if="data.length === 0 && loading">
+    Loading...
+  </div>
+  <div v-else>
+    <table class="table">
+      <template v-if="data.length > 0">
+        <thead>
+          <th v-for="column in columns" :key="column.title">
+            {{ column.title }}
+          </th>
+        </thead>
+        <tbody>
+          <tr v-for="item in data" :key="item.id">
+            <td
+              v-for="column in columns"
+              :key="column.title"
+              :data-label="column.title"
+            >
+              <div v-if="!column.customRender">
+                {{ item[column.dataIndex] }}
+              </div>
+              <div v-else>
+                <slot :name="column.customRender" :value="item" />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+      <div class="info" v-else>
+        No data
+      </div>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
@@ -42,61 +54,95 @@ export default defineComponent({
       >,
       required: true,
     },
+    loading: Boolean,
     withPagination: Boolean,
   },
 });
 </script>
 
 <style lang="scss">
+.info {
+  width: 100%;
+  text-align: center;
+}
+
 .table {
   width: 100%;
-  border-collapse: collapse;
-}
+  border-spacing: 0px;
+  border-collapse: separate;
 
-.table td,
-.table th {
-  padding: 16px;
-  border-bottom: 1px solid $gray-light;
-  font-size: 16px;
-  color: $gray;
-}
-
-.table th {
-  background-color: rgba(229, 234, 244, 0.25);
-  text-align: left;
-  font-weight: 500;
-  border: none;
-}
-
-@media (max-width: 820px) {
-  .table thead {
-    display: none;
+  tr {
+    height: 82px;
   }
 
-  .table,
-  .table tbody,
-  .table tr,
-  .table td {
+  td,
+  th {
+    padding: 16px;
+    border-bottom: 1px solid $gray-light;
+    font-size: 16px;
+    color: $gray;
+  }
+
+  th {
+    background-color: rgba(229, 234, 244, 0.25);
+    text-align: left;
+    font-weight: 500;
+    border: none;
+  }
+
+  @media (min-width: $lg) {
+    tr td,
+    thead th {
+      &:first-child {
+        @include wrapperGutterLeft;
+      }
+
+      &:last-child {
+        @include wrapperGutterRight;
+      }
+    }
+  }
+}
+
+@media (max-width: $lg) {
+  .table {
+    table-layout: auto;
+    border-collapse: collapse;
     display: block;
     width: 100%;
-  }
-  .table tr {
-    margin-bottom: 15px;
-  }
-  .table td {
-    padding-left: 50%;
-    text-align: left;
-    position: relative;
-  }
-  .table td::before {
-    content: attr(data-label);
-    position: absolute;
-    left: 0;
-    width: 50%;
-    padding-left: 15px;
-    font-size: 15px;
-    font-weight: bold;
-    text-align: left;
+
+    tr {
+      height: auto;
+      margin-bottom: 15px;
+    }
+
+    thead {
+      display: none;
+    }
+
+    tbody,
+    tr,
+    td {
+      display: block;
+      width: 100%;
+    }
+
+    td {
+      padding-left: 50%;
+      text-align: left;
+      position: relative;
+
+      &::before {
+        content: attr(data-label);
+        position: absolute;
+        left: 0;
+        width: 50%;
+        padding-left: 15px;
+        font-size: 15px;
+        font-weight: bold;
+        text-align: left;
+      }
+    }
   }
 }
 </style>
